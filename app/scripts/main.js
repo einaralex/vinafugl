65e2b9db361adfd93926a6fefa76e31eda646a19
Þórdís Jóna <thordisj12@ru.is>
2014-04-02 19:32:34 +0000

    Fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,9 @@
-
 /**
  * Bootstrap and start the game.
  */
 $(function() {
     'use strict';
-
+    
     var game = new window.Game($('.GameCanvas'));
     console.log('Starting game:');
     console.log(game);
